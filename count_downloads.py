4617045bc036213f013ff7bc4f763d4b8b2e6e16
Umberto Villa <uvilla@ices.utexas.edu>
2017-06-28 13:23:03 -0500

    Fix license in notebooks

--- count_downloads.py
+++ count_downloads.py
@@ -8,11 +8,13 @@ import urllib
 
 #key = 'AIzaSyCnCjlxS5LUvP1jjXansnk1ymt-Liga2aQ'
 key = 'AIzaSyBLuHKVMU8kxU5R2ykwEgGTdhuINg-3HUE'
-versions = [ ('v1.0.0', 'https://goo.gl/h7HUFJ'),
-           ('v1.0.1', 'https://goo.gl/srnvsc'),
-           ('v1.0.2', 'https://goo.gl/wMb18C'),
-           ('v1.1.0', 'https://goo.gl/pDb10B'),
-           ('v1.2.0', 'https://goo.gl/OcvROZ')]
+versions = [('v1.3.0', 'https://goo.gl/NgJ887'),
+            ('v1.2.0', 'https://goo.gl/OcvROZ'),
+            ('v1.1.0', 'https://goo.gl/pDb10B'),
+            ('v1.0.2', 'https://goo.gl/wMb18C'),
+            ('v1.0.1', 'https://goo.gl/srnvsc'),
+            ('v1.0.0', 'https://goo.gl/h7HUFJ')
+           ]
 
 request = 'https://www.googleapis.com/urlshortener/v1/url?shortUrl={0}&projection=FULL&key={1}'
 
--- src/tutorials/1_FEniCS101/index.md
+++ src/tutorials/1_FEniCS101/index.md
@@ -340,4 +340,4 @@ See file COPYRIGHT for details.
 This file is part of the hIPPYlib library. For more information and source code
 availability see https://hippylib.github.io.
 
-hIPPYlib is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (as published by the Free Software Foundation) version 3.0 dated June 2007.
+hIPPYlib is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License (as published by the Free Software Foundation) version 2.0 dated June 1991.
